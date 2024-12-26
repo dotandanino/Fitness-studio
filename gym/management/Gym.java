b931d2da68055162639d2ac8aa9978e72afcd7a2
dotandanino <dotandanino@gmail.com>
2024-12-26 20:20:52 +0200

    initial commit

--- gym/management/Gym.java
+++ gym/management/Gym.java
@@ -1,14 +1,25 @@
 package gym.management;
+import gym.management.Session;
+import gym.customers.*;
 
-import gym.customers.Person;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Gym {
     private static Gym gym;
     private static String name;
     private Secretary secretary;
     int balance=0;
+    private int gymBalance = 0;
+    ArrayList<Session> sessions = new ArrayList<>();
+    ArrayList<Instructor> instructors = new ArrayList<>();
+    ArrayList<Client> clients=new ArrayList<>();
+    ArrayList<String>  notifications = new ArrayList<>();
     private Gym(){
 
+    }
+    void addToBalnce(int i){
+        this.gymBalance+=i;
     }
     public static Gym getInstance() {
         if(gym==null){
@@ -22,8 +33,11 @@ public class Gym {
     }
 
     public void setSecretary(Person p1, int i) {
+        if(this.secretary!=null)
+            this.secretary.isCurrent=false;
         this.secretary=new Secretary(p1,i);
-        secretary.addNotification("A new secretary has started working at the gym: "+p1.getName());
+        this.secretary.isCurrent=true;
+        this.notifications.add("A new secretary has started working at the gym: "+p1.getName());
     }
     public Secretary getSecretary() {
         return secretary;
@@ -32,16 +46,22 @@ public class Gym {
     public String toString(){
         String str="Gym Name: "+name+"\n";
         str+="Gym Secretary: "+secretary.toString()+"\n";
-        str+="Gym Balance: "+secretary.getGymBalance()+"\n\n";
+        str+="Gym Balance: "+gymBalance+"\n\n";
         str+="Clients Data:\n";
-        str+=secretary.clientsPrint();
+        for (Client c: clients){
+            str+=c.toString()+"\n";
+        }
         str+="\n";
         str+="Employees Data:\n";
-        str+=secretary.instructursPrint();
+        for (Instructor i: instructors){
+            str+=i.toString()+"\n";
+        }
         str+=secretary.toString()+"\n";
         str+="\n";
         str+="Sessions Data:";
-        str+=secretary.sessionPrint();
+        for (Session s:sessions){
+            str+="\n"+s.toString();
+        }
         return str;
     }
 }
--- gym/management/Secretary.java
+++ gym/management/Secretary.java
@@ -13,16 +13,10 @@ import java.util.List;
 
 public class Secretary extends Person{
     private int salary;
+    boolean isCurrent;
     /**
      * the next are static because i want to keep this data in case we switch secretary;
      */
-    static int trying=0;
-    private static int gymBalance = 0;
-    private static List<Session> sessions = new ArrayList<>();
-    private static List<Instructor> instructors = new ArrayList<>();
-    private static ArrayList<Client> clients=new ArrayList<>();
-    private static ArrayList<String>  notifications = new ArrayList<>();
-
     public Secretary(Person person, int salary) {
         super(person);
         this.salary = salary;;
@@ -32,31 +26,31 @@ public class Secretary extends Person{
      * check if this secretary equals to current gym secretary.
      * @return if this is the current secretary of from the past
      */
-    private boolean isCurrent(){
-        return this.equals(Gym.getInstance().getSecretary());
-    }
     public Client registerClient(Person p) {
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
         int age = Dates.ageCalculator(p);
+        Gym gym=Gym.getInstance();
         if (age < 18)
             throw new InvalidAgeException("Error: Client must be at least 18 years old to register");
         Client c = new Client(p);
-        if (clients.contains(c)) {
+        if (gym.clients.contains(c)) {
             throw new DuplicateClientException("Error: The client is already registered");
         }
-        clients.add(c);
-        notifications.add("Registered new client: " + c.getName());
+        gym.clients.add(c);
+        gym.notifications.add("Registered new client: " + c.getName());
         return c;
     }
 
     public void unregisterClient(Client c2) {
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
+        Gym gym=Gym.getInstance();
+        ArrayList<Client> clients=gym.clients;
         for (int i = 0; i < clients.size(); i++) {
             if (clients.get(i).equals(c2)) {
                 clients.remove(i);
-                notifications.add("Unregistered client: " + c2.getName());
+                gym.notifications.add("Unregistered client: " + c2.getName());
                 return;
             }
         }
@@ -64,41 +58,44 @@ public class Secretary extends Person{
     }
 
     public Instructor hireInstructor(Person p, int i, ArrayList<SessionType> sessionTypes) {
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
-        notifications.add("Hired new instructor: " + p.getName() + " with salary per hour: " + i);
+        Gym gym=Gym.getInstance();
+        gym.notifications.add("Hired new instructor: " + p.getName() + " with salary per hour: " + i);
         Instructor I = new Instructor(p, i, sessionTypes);
-        instructors.add(I);
+        gym.instructors.add(I);
         return I;
     }
 
     public Session addSession(SessionType type, String date, ForumType ft, Instructor i) {
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
+        Gym gym=Gym.getInstance();
         if (i.getSessionTypes().contains(type)) {
             Session session = sessionFactory.createSession(type, date, ft, i);
-            sessions.add(session);
-            notifications.add("Created new session: " + type + " on " + session.getDate().toString() + " with instructor: " + i.getName());
+            gym.sessions.add(session);
+            gym.notifications.add("Created new session: " + type + " on " + session.getDate().toString() + " with instructor: " + i.getName());
             return session;
         }
         throw new InstructorNotQualifiedException("Error: Instructor is not qualified to conduct this session type.");
     }
 
     public void registerClientToLesson(Client c, Session s){
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
+        Gym gym=Gym.getInstance();
         boolean possible = true;
         LocalDateTime sessionDate = s.getDate();
         LocalDateTime now = LocalDateTime.now();
         if (s.getClients().contains(c)) {
             throw new DuplicateClientException("Error: The client is already registered for this lesson");
         }
-        if (!clients.contains(c)) {
+        if (!gym.clients.contains(c)) {
             throw new ClientNotRegisteredException("Error: The client is not registered with the gym and cannot enroll in lessons");
         }
         if (sessionDate.isBefore(now)) {
             possible = false;
-            notifications.add("Failed registration: Session is not in the future");
+            gym.notifications.add("Failed registration: Session is not in the future");
         }
         if (!c.getForumTypes().contains(s.getForumType())) {
             possible = false;
@@ -108,32 +105,33 @@ public class Secretary extends Person{
             } else {
                 str = "Failed registration: Client doesn't meet the age requirements for this session (Seniors)";
             }
-            notifications.add(str);
+            gym.notifications.add(str);
         }
         if (s.getClients().size() == s.getCapacity()) {
             possible = false;
-            notifications.add("Failed registration: No available spots for session");
+            gym.notifications.add("Failed registration: No available spots for session");
         }
         if (c.getBalance() < s.getPrice()) {
             possible = false;
-            notifications.add("Failed registration: Client doesn't have enough balance");
+            gym.notifications.add("Failed registration: Client doesn't have enough balance");
         }
 
         if (possible) {
             s.getClients().add(c);
             c.addToBalance(-s.getPrice());
-            gymBalance += s.getPrice();
-            notifications.add("Registered client: " + c.getName() + " to session: " + s.getSessionType() + " on " + s.getDate().toString() + " for price: " + s.getPrice());
+            gym.addToBalnce(s.getPrice());
+            gym.notifications.add("Registered client: " + c.getName() + " to session: " + s.getSessionType() + " on " + s.getDate().toString() + " for price: " + s.getPrice());
         }
     }
 
     public void paySalaries() {
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
         int counter=0;
         Instructor instructor;
         int salary;
-        for (Session s:sessions){
+        Gym gym=Gym.getInstance();
+        for (Session s:gym.sessions){
             instructor=s.getI();
             salary=instructor.getSalary();
             instructor.addToBalance(salary);
@@ -141,35 +139,35 @@ public class Secretary extends Person{
         }
         counter+=this.salary;
         this.addToBalance(this.salary);
-        this.gymBalance -=counter;
-        notifications.add("Salaries have been paid to all employees");
-    }
-    protected int getGymBalance(){
-        return gymBalance;
+        gym.addToBalnce(-counter);
+        gym.notifications.add("Salaries have been paid to all employees");
     }
     public void printActions() {
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
-        for (String str : notifications) {
+        Gym gym=Gym.getInstance();
+        for (String str : gym.notifications) {
             System.out.println(str);
         }
     }
     public void notify(Session s, String str) {
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
         NewsletterPublisher newsletterPublisher = new NewsletterPublisher();
         for (Client c : s.getClients()) {
             newsletterPublisher.register(c);
         }
+        Gym gym=Gym.getInstance();
         newsletterPublisher.notifyMembers(str);
-        notifications.add("A message was sent to everyone registered for session "+s.getSessionType()+" on "+s.getDate()+" : "+str);
+        gym.notifications.add("A message was sent to everyone registered for session "+s.getSessionType()+" on "+s.getDate()+" : "+str);
     }
 
     public void notify(String date, String str) {
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
         NewsletterPublisher newsletterPublisher = new NewsletterPublisher();
-        for (Session s:sessions){
+        Gym gym=Gym.getInstance();
+        for (Session s:gym.sessions){
             if((s.getStrDate().substring(0,10)).equals(date)){
                 for (Client c : s.getClients()) {
                     newsletterPublisher.register(c);
@@ -178,55 +176,22 @@ public class Secretary extends Person{
         }
         newsletterPublisher.sendNewsletter(str);
 
-        notifications.add("A message was sent to everyone registered for a session on "+Dates.toLocal(date)+" : "+str);
+        gym.notifications.add("A message was sent to everyone registered for a session on "+Dates.toLocal(date)+" : "+str);
     }
     public void notify(String str) {
-        if(!this.isCurrent())
+        if(!this.isCurrent)
             throw new NullPointerException();
+        Gym gym=Gym.getInstance();
         NewsletterPublisher newsletterPublisher = new NewsletterPublisher();
-        for (Client c : clients) {
+        for (Client c : gym.clients) {
             newsletterPublisher.register(c);
         }
         newsletterPublisher.notifyMembers(str);
-        notifications.add("A message was sent to all gym clients: " + str);
-    }
-
-    void addNotification(String s) {
-        if(!this.isCurrent())
-            throw new NullPointerException();
-        notifications.add(s);
+        gym.notifications.add("A message was sent to all gym clients: " + str);
     }
 
     @Override
     public String toString() {
         return super.toString() + " | Role: Secretary | Salary per Month: " + salary;
-    }
-
-    public String instructursPrint() {
-        if(!this.isCurrent())
-            throw new NullPointerException();
-        String str="";
-        for (Instructor i: instructors){
-            str+=i.toString()+"\n";
-        }
-        return str;
-    }
-    public String sessionPrint(){
-        if(!this.isCurrent())
-            throw new NullPointerException();
-        String str="";
-        for (Session s:sessions){
-            str+="\n"+s.toString();
-        }
-        return str;
-    }
-    public String clientsPrint(){
-        if(!this.isCurrent())
-            throw new NullPointerException();
-        String str="";
-        for (Client c: clients){
-            str+=c.toString()+"\n";
-        }
-        return str;
     }
 }
